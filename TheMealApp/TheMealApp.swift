2bb4bd35a824c85879a9af87e81cdf814dee1d14
Romain Gréaume <r.greaume@beapp.fr>
2023-03-29 09:34:04 +0200

    init project

---
+++ TheMealApp/TheMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealAppApp.swift
+//  TheMealApp
+//
+//  Created by Romain Gréaume on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
